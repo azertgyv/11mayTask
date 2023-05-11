4a7699fc96826437ae9e67b227eecb216b67c798
azertgyv <atagiyev077@gmail.com>
2023-05-11 13:27:30 +0400

    11mayTask

---
+++ app.js
@@ -0,0 +1,51 @@
+// TASK 3
+
+// const nums = (par) => {
+//     let newNum = []
+//     for (let i = 100; i < 1000; i++) {
+//         if (i % par === 0) {
+//             newNum.push(i)
+//         }
+//     }
+//     return newNum
+// }
+// console.log(nums(50))
+// console.log(nums(70))
+// console.log(nums(85))
+
+// -----------------------------------------------------------
+
+// TASK 5
+
+// let newNum = [1, 4, 7, 8, 3, 5, 0, 2, 4, 6, 12, 15, 18, 46, 45, 48, 97, 84]
+// const nums = (par) => {
+//     let evens = 0
+//     let odds = 0
+//     for (let i = 0; i < par.length; i++) {
+//         if (par[i] % 2 === 0) {
+//             evens++
+//         } else {
+//             odds++
+//         }
+//     }
+//     return { evens, odds }
+// }
+// console.log(nums(newNum))
+
+// --------------------------------------------------------------
+
+// TASK 4
+
+// let str = 'aBcDfG,JaVaStRiPt'
+// let result = ''
+// for (i = 0; i < str.length; i++) {
+//     let char = str.charAt(i);
+//     if (char >= 'a' && char <= 'z') {
+//         char = char.toUpperCase()
+//     } else if ((char >= 'A' && char <= 'Z')) {
+//         char = char.toLowerCase()
+//     }
+//     result += char;
+// }
+// console.log(result)
+
